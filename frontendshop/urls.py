438973e90dc72433b85e9a428b1b402d1a8b4f41
Ran-oops <3460466167@qq.com>
2020-02-01 22:46:21 +0800

    forfrontend web

--- frontendshop/urls.py
+++ frontendshop/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.conf.urls import include, url
 from . import views
+
 urlpatterns = [
     url(r'^index$', views.index, name="index"),
     url(r'^login$', views.login, name="login"),
     url(r'^logout$', views.logout, name="logout"),
     url(r'^dologin$', views.dologin, name='dologin'),
     url(r'^list$', views.list, name='list'),
+    url(r'^detail/(?P<tid>[0-9]+)$', views.detail, name='detail'),
     url(r'^draw$', views.draw_certificate, name='draw_certificate'),
-	url(r'^vipusers$', views.vipusers, name='vipusers'),
-	url(r'^shoppingCart$', views.shoppingCart, name='shoppingCart'),
-	
-
+    url(r'^vipusers$', views.vipusers, name='vipusers'),
+    url(r'^shoppingCart$', views.shoppingCart, name='shoppingCart'),
+    url(r'^shopcartadd/(?P<tid>[0-9]+)$', views.shopcartadd, name='shopcartadd')
 ]
--- frontendshop/views.py
+++ frontendshop/views.py
@@ -3,24 +3,27 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from frontendshop.models import OneTypeGoodsOrder, GoodsInfo, CGoodsType, PGoodsType, GuestInfo
+
+
 # Create your views here.
 
-#homepage
+# homepage
 def index(request):
-    return render(request,'frontend/index.html')
+    return render(request, 'frontend/index.html')
+
 
 def login(request):
     return render(request, 'frontend/login.html')
 
 
 def dologin(request):
-    userphone=request.POST['usernum']
-    userpassword=request.POST['password']
-    certificateCode=request.POST['cCode']
-    user=GuestInfo.objects.get(phonenumber=userphone)
+    userphone = request.POST['usernum']
+    userpassword = request.POST['password']
+    certificateCode = request.POST['cCode']
+    user = GuestInfo.objects.get(phonenumber=userphone)
     print(userphone)
     print(userpassword)
-    print('certificateCode from html:',certificateCode)
+    print('certificateCode from html:', certificateCode)
 
     # import hashlib
     # m=hashlib.md5()
@@ -30,7 +33,7 @@ def dologin(request):
     # print("userpassword:", user.password)
     # if user.password==m.hexdigest():
     if certificateCode == request.session['certificateCode']:
-        if user.password==userpassword:
+        if user.password == userpassword:
             request.session['vipuser'] = user.to_dict()
             # print('this is vipuser=================', request.session['vipuser'])
             return redirect(reverse('index'))
@@ -38,63 +41,64 @@ def dologin(request):
             return HttpResponse('ERROR')
     else:
         return HttpResponse('certificate is wrong!')
+
+
 def draw_certificate(request):
     from PIL import Image, ImageDraw, ImageFont
     import random
-    bgcolor = (random.randrange(20,100), random.randrange(20,100), 255)
+    bgcolor = (random.randrange(20, 100), random.randrange(20, 100), 255)
     width = 100
     height = 25
-    #create picture object
-    im = Image.new('RGB',(width,height), bgcolor)
+    # create picture object
+    im = Image.new('RGB', (width, height), bgcolor)
 
-    print('this is im:',im)
-    #create pen object
+    print('this is im:', im)
+    # create pen object
     draw_pen = ImageDraw.Draw(im)
-    #call point() function of Draw_pen to draw some point
-    for i in range(0,100):
+    # call point() function of Draw_pen to draw some point
+    for i in range(0, 100):
         xy = (random.randrange(0, width), random.randrange(0, height))
-        fill = (random.randrange(0,255),255, random.randrange(0,255))
-        draw_pen.point(xy, fill = fill)
-    #定义验证码的备选值
+        fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
+        draw_pen.point(xy, fill=fill)
+    # 定义验证码的备选值
     str1 = 'ABCDEFGHI1234JKLMNOPQRS890TUVWXYZ567'
     rand_str = ''
-    for i in range(0,4):
+    for i in range(0, 4):
         rand_str += str1[random.randrange(0, len(str1))]
         # print('this is rand_str:',rand_str)
 
-    #create font object
+    # create font object
     font = ImageFont.load_default().font
 
-    fontcolor = (255, random.randrange(0,255), random.randrange(0,255))
+    fontcolor = (255, random.randrange(0, 255), random.randrange(0, 255))
 
-    #to draw four letters
-    draw_pen.text((10,2),rand_str[0],font=font,fill=fontcolor)
-    draw_pen.text((35,2),rand_str[1],font=font,fill=fontcolor)
-    draw_pen.text((60,2),rand_str[2],font=font,fill=fontcolor)
-    draw_pen.text((85,2),rand_str[3],font=font,fill=fontcolor)
+    # to draw four letters
+    draw_pen.text((10, 2), rand_str[0], font=font, fill=fontcolor)
+    draw_pen.text((35, 2), rand_str[1], font=font, fill=fontcolor)
+    draw_pen.text((60, 2), rand_str[2], font=font, fill=fontcolor)
+    draw_pen.text((85, 2), rand_str[3], font=font, fill=fontcolor)
 
     del draw_pen
-    request.session['certificateCode']=rand_str
+    request.session['certificateCode'] = rand_str
     # print('rand_str:', rand_str)
     print('restored certificate:', request.session['certificateCode'])
 
     # 内存文件操作-->此方法为python3的
     import io
     buf = io.BytesIO()
-    #将图片保存在内存中，文件类型为png
+    # 将图片保存在内存中，文件类型为png
     im.save(buf, 'png')
-    #将内存中的图片数据返回给客户端，MIME类型为图片png
+    # 将内存中的图片数据返回给客户端，MIME类型为图片png
     return HttpResponse(buf.getvalue(), 'image/png')
 
 
 def logout(request):
-	del request.session['vipuser']
+    del request.session['vipuser']
     # return HttpResponse('HELLO')
-	return redirect(reverse('index'))
+    return redirect(reverse('index'))
 
 
-
-#goods_list page
+# goods_list page
 
 def list(request):
     goodsList = {}
@@ -104,16 +108,26 @@ def list(request):
     for item in goods_info:
         print(item.picname)
 
-    goodsList['context']=goods_info
+    goodsList['context'] = goods_info
 
     return render(request, 'frontend/list.html', goodsList)
-	
 
-#user detail information index page
+
+def detail(request, tid):
+    goods = GoodsInfo.objects.filter(id=tid)
+    return render(request, 'frontend/detail.html', {'goods': goods})
+    # return HttpResponse('hi, detail')
+
+
+# user detail information index page
 def vipusers(request):
-	return HttpResponse('this is me')
+    return HttpResponse('this is me')
 
 
-#user's shopping-cart page
+def shopcartadd(request, tid):
+    return HttpResponse('hi, add to cart')
+
+
+# user's shopping-cart page
 def shoppingCart(request):
-	return HttpResponse('my shopping-cart')
+    return HttpResponse('my shopping-cart')
